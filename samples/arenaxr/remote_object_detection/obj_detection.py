01b34bcab28f27da0a820ab5a5e278fe8f2b6404
1230212 <1230212@isep.ipp.pt>
2025-03-19 00:25:51 +0000

    arenaxr - remote object detection sending detection results

---
+++ samples/arenaxr/remote_object_detection/obj_detection.py
@@ -0,0 +1,60 @@
+import logging
+import time
+from queue import Queue
+
+import cv2
+import torch
+from ultralytics import YOLO
+
+
+def process_frame(last_frame, new_frame):
+    if last_frame is None:
+        return True
+
+    gray_last = cv2.cvtColor(last_frame, cv2.COLOR_BGR2GRAY)
+    gray_current = cv2.cvtColor(new_frame, cv2.COLOR_BGR2GRAY)
+    gray_current = cv2.resize(gray_current, (gray_last.shape[1], gray_last.shape[0]))
+
+    diff = cv2.absdiff(gray_last, gray_current)
+    non_zero_count = cv2.countNonZero(diff)
+
+    if non_zero_count / diff.size < 0.5:
+        return False
+    return True
+
+
+def detect_objects(frame_queue: Queue, results_queue: Queue):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    logging.info(f"Using {device} for object detection")
+
+    model = YOLO("yolov8n.pt").to(device)
+    last_frame = None
+    frame_count = 0
+
+    while True:
+        time.sleep(0.1)
+        if not frame_queue.empty():
+            frame = frame_queue.get()
+            h, w, _ = frame.shape
+            print(h, w)
+
+            if not process_frame(last_frame, frame):
+                continue
+
+            last_frame = frame
+            frame_count += 1
+            logging.info(f"Processing frame {frame_count}")
+            results = model(frame)
+
+            detection_result = []
+            for result in results:
+                for box in result.boxes:
+                    x1, y1, x2, y2 = map(int, box.xyxy[0])
+
+                    #x1_norm, y1_norm = x1 / w, y1 / h
+                    #x2_norm, y2_norm = x2 / w, y2 / h
+
+                    label = model.names[int(box.cls[0])]
+                    detection_result.append((label, x1, y1, x2, y2))
+
+            results_queue.put(detection_result)
--- samples/arenaxr/remote_object_detection/server.py
+++ samples/arenaxr/remote_object_detection/server.py
@@ -2,19 +2,23 @@ import asyncio
 import json
 import logging
 import queue
+import ssl
 from threading import Thread
 
 import aiohttp_cors
-import cv2
 from aiohttp import web
-from aiortc import MediaStreamTrack, RTCPeerConnection, RTCSessionDescription
-from av.video.frame import VideoFrame
+from aiortc import MediaStreamTrack, RTCPeerConnection, RTCSessionDescription, RTCDataChannel
+from av import VideoFrame
 
-from annotation_renderer.obj_detection import detect_objects
+from remote_object_detection.obj_detection import detect_objects
+
+ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+ssl_context.load_cert_chain(certfile="cert.pem", keyfile="key.pem")
 
 logging.basicConfig(level=logging.INFO)
 
 frame_queue = queue.Queue()
+results_queue = queue.Queue()
 
 
 async def handle_video_track(track: MediaStreamTrack):
@@ -24,13 +28,11 @@ async def handle_video_track(track: MediaStreamTrack):
         try:
             frame_count += 1
             frame = await asyncio.wait_for(track.recv(), timeout=30)
-            #if isinstance(frame, VideoFrame):
-            frame = frame.to_ndarray(format="bgr24")
+            if isinstance(frame, VideoFrame):
+                frame = frame.to_ndarray(format="bgr24")
 
             frame_queue.put(frame)
             logging.info(f"Enqueued frame {frame_count}")
-            # cv2.imwrite(f"imgs/received_frame_{frame_count}.jpg", frame)
-
         except asyncio.TimeoutError:
             logging.warning("Timeout: No frames received, closing stream.")
             break
@@ -39,9 +41,24 @@ async def handle_video_track(track: MediaStreamTrack):
             break
 
 
+async def send_obj_coordinates(channel: RTCDataChannel):
+    while True:
+        await asyncio.sleep(0.1)
+        if not results_queue.empty():
+            json_data = json.dumps(results_queue.get())
+            try:
+                channel.send(json_data)
+                logging.info(f"Sent data: {json_data}")
+            except Exception as e:
+                logging.error(f"Error sending data: {e}")
+                break
+
+
 async def handle_offer(request):
     body = await request.json()
     pc = RTCPeerConnection()
+
+    channel = pc.createDataChannel("obj_detection")
 
     @pc.on("track")
     def on_track(track):
@@ -49,13 +66,14 @@ async def handle_offer(request):
             logging.info(f"Receiving {track.kind} track")
             asyncio.ensure_future(handle_video_track(track))
 
-    @pc.on("datachannel")
-    def on_datachannel(channel):
-        logging.info(f"Data channel established: {channel.label}")
-
     @pc.on("connectionstatechange")
     async def on_connectionstatechange():
         logging.info(f"Connection state is {pc.connectionState}")
+
+    @channel.on("open")
+    def on_channel():
+        logging.info(f"Data channel established: {channel.label}")
+        asyncio.ensure_future(send_obj_coordinates(channel))
 
     offer = RTCSessionDescription(sdp=body["sdp"], type=body["type"])
     await pc.setRemoteDescription(offer)
@@ -81,5 +99,5 @@ route = app.router.add_post("/offer", handle_offer)
 cors.add(route)
 
 if __name__ == "__main__":
-    Thread(target=detect_objects, args=(frame_queue,)).start()
-    web.run_app(app, port=8181)
+    Thread(target=detect_objects, args=(frame_queue, results_queue)).start()
+    web.run_app(app, port=8181, ssl_context=ssl_context)
